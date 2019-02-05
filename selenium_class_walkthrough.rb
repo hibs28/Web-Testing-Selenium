c07f404f2287cbd44bc277f103cd2f117b5c9158
Tech-A06 <hibah28@live.co.uk>
2019-02-05 11:26:46 +0000

    created a method which find the text field and enters a text

--- selenium_class_walkthrough.rb
+++ selenium_class_walkthrough.rb
@@ -1,11 +1,18 @@
 require 'selenium-webdriver'
 
 class QaToolsForm
+  #page objects
+  PRACTICE_FORM_URL = 'https://www.toolsqa.com/automation-practice-form/'
+  FIRST_NAME_FIELD_NAME = 'firstname'
   def initialize
     @chrome_driver = Selenium::WebDriver.for :chrome
   end
 
   def visit_practice_form
-    @chrome_driver.get('https://www.toolsqa.com/automation-practice-form/')
+    @chrome_driver.get(PRACTICE_FORM_URL)
+  end
+
+  def input_firstname_field(text)
+    @chrome_driver.find_element(:name, FIRST_NAME_FIELD_NAME).send_keys(text)
   end
 end
