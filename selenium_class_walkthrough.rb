75b5980c6b2b538b18e49a0e80bc676597bba8c8
Tech-A06 <hibah28@live.co.uk>
2019-02-05 10:59:05 +0000

    created a method(visit_practice_form) which will open and load the webpage

--- selenium_class_walkthrough.rb
+++ selenium_class_walkthrough.rb
@@ -4,5 +4,8 @@ class QaToolsForm
   def initialize
     @chrome_driver = Selenium::WebDriver.for :chrome
   end
-  
+
+  def visit_practice_form
+    @chrome_driver.get('https://www.toolsqa.com/automation-practice-form/')
+  end
 end
